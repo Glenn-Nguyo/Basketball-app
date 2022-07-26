d71faca5458f101968c464d472eb9ffd08d651dc
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-26 15:49:49 +0300

    Use-Effect

--- basketball-app/src/Component/Leagues.js
+++ basketball-app/src/Component/Leagues.js
@@ -1,32 +1,29 @@
 import React, {useState, useEffect} from "react";
 import "../App";
 import axios from "axios";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 function Leagues(){
     const [data, setData] = useState([]);
-    useEffect(()=> {
-        const options = {
-            method: 'GET',
-            url: 'https://api-basketball.p.rapidapi.com/timezone',
-            headers: {
-              'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-              'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
-            }
-          };
-          
-          axios.request(options).then(function (response) {
-              console.log(response.data);
-          }).catch(function (error) {
-              console.error(error);
-          });
-    },[])
+    useEffect(() => {
+        axios
+          .get("https://api-basketball.p.rapidapi.com/leagues")
+          .then((response) => {
+            console.log(response.data.data);
+            setData(response.data.data);
+          })
+          .catch((err) => console.log(err));
+      }, []);
 
     return(
         <div className="leagues">
-         leagues
-        </div>
+        {data?.map((league) => (
+          <div key={league.id} className="league-div">
+            <img src={league.logos.light} alt="#" />
+            <h4>{league.name}</h4>
+          </div>
+        ))}
+      </div>
     )
 }
 
