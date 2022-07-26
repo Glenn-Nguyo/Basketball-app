34631aa5af87be89aeb4942e367125f231c0465c
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-26 13:03:21 +0300

    second

---
+++ basketball-app/src/Component/Content.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css'
+
+function Content (){
+    return(
+    <div className='content'>
+        <div className='tabs'>
+            <div className='tab-leagues'>
+                <h2>Leagues</h2>
+            </div>
+            <div className='tab-standings'>
+                <h2>Standings</h2>
+            </div>
+        </div>
+
+    </div>
+    )
+}
+
+export default Content;
---
+++ basketball-app/src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../App.css'
+
+function Footer(){
+ return(
+    <div className="footer">
+ <p>Basketball App 2022</p>
+    </div>
+ )
+}
+export default Footer;
