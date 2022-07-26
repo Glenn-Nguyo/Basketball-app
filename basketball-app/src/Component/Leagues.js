47ba122b107b56f70d4752924880b811ffa23f13
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-26 15:00:09 +0300

    Imports-leagues

--- basketball-app/src/Component/Leagues.js
+++ basketball-app/src/Component/Leagues.js
@@ -1,4 +1,8 @@
 import React from "react";
+import "../App";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function Leagues(){
     return(
