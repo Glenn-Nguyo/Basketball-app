bdab18c270a5ff2cc3f209ca62442cad72a7d736
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-26 15:07:26 +0300

    Use-Effect

--- basketball-app/src/Component/Leagues.js
+++ basketball-app/src/Component/Leagues.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../App";
 import axios from "axios";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 function Leagues(){
+    const [data, setData] = useState([]);
+    useEffect(()=> {
+        const options = {
+            method: 'GET',
+            url: 'https://api-basketball.p.rapidapi.com/timezone',
+            headers: {
+              'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+              'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+              console.log(response.data);
+          }).catch(function (error) {
+              console.error(error);
+          });
+    },[])
+
     return(
         <div className="leagues">
          leagues
